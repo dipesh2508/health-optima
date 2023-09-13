09ab77dc8fa6b19c5f858d21764c1f47a318beea
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-09-13 22:59:44 +0530

    navbar v1

---
+++ src/app/apps/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const apps = () => {
+  return (
+    <div>apps</div>
+  )
+}
+
+export default apps
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,17 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'primary': '#A855F7',
+        'secondary': '#115E59',
+        'background': '#FFFBEB',
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+        serif: ['PT Serif', 'serif']
+      },
     },
-  },
-  plugins: [],
+    plugins: [],
+  }
 }
 export default config
