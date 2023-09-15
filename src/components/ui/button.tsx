6fcfdabe31f81866d582a3dfa808e43187635546
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-09-15 23:11:42 +0530

    error 404 fixed

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,13 +9,13 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-white p-2 border-2 ease-in-out duration-150 hover:border-primary hover:text-primary hover:bg-transparent",
+        default: "bg-primary text-white p-2 border-2 ease-in-out duration-250 hover:border-primary hover:text-primary hover:bg-transparent",
         destructive:
           "bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90",
         outline:
           "border border-slate-200 bg-white hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50",
         secondary:
-          "bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
+          "bg-secondary text-white p-2 border-2 ease-in-out duration-250 hover:border-secondary hover:text-secondary hover:bg-transparent",
         ghost: "hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
         link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
       },
