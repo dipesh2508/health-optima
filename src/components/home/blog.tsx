720e64d7cc97acd0089c05a062b011b0647346ff
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-09-19 19:43:36 +0530

    blog section home

--- src/components/home/blog.tsx
+++ src/components/home/blog.tsx
@@ -1,5 +1,47 @@
 import React from 'react'
+import Image from 'next/image'
+import { Button } from '../ui/button'
 
+import blog1 from '@/assets/images/blog 1.png'
+import blog2 from '@/assets/images/blog 2.png'
+import blog3 from '@/assets/images/blog 3.png'
+import blog4 from '@/assets/images/blog 4.png'
+
+const data = [
+    {
+        no: '01',
+        image: blog1,
+        category: 'category',
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est.',
+        link: '#'
+    }
+    ,
+    {
+        no: '02',
+        image: blog2,
+        category: 'category',
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est.',
+        link: '#'
+    },
+    {
+        no: '03',
+        image: blog3,
+        category: 'category',
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est.',
+        link: '#'
+    },
+    {
+        no: '04',
+        image: blog4,
+        category: 'category',
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est.',
+        link: '#'
+    }
+]
 const blog = () => {
     return (
         <section className='min-h-screen mt-28 flex flex-col items-center'>
@@ -7,8 +49,29 @@ const blog = () => {
                 Take a <span className='text-secondary'>Look</span>
             </h1>
 
-            <div>
+            <div className='grid grid-cols-2 gap-12 mx-24 mt-28 content center'>
+                {data.map((item, index) => (
 
+                        <div className='grid grid-cols-2 content-center align-middle gap-3' key={index}>
+                            <div className='grid grid-1 content-center'>
+                                <Image height={34} width={473} src={item.image} alt="" className='rounded-md hover:drop-shadow-lg' />
+                            </div>
+                            <div className='flex flex-col justify content-center'>
+
+                                <h1 className='text-base font-sans font-thin '>
+                                    {item.category}
+                                </h1>
+                                <h1 className='text-2xl font-serif leading-6 font-semibold'>
+                                    {item.title}
+                                </h1>
+                                <h1 className='text-xs font-sans font-light mt-1'>
+                                    {item.description}
+                                </h1>
+                                <div><Button variant={'secondary'} size={'sm'} className='mt-2'>Read</Button></div>
+                                
+                            </div>
+                    </div>
+                ))}
             </div>
         </section>
 
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,19 +9,19 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-white p-2 border-2 ease-in-out duration-250 hover:border-primary hover:text-primary hover:bg-transparent",
+        default: "bg-primary text-white p-2 border-2 border-primary ease-in-out duration-250 hover:border-primary hover:text-primary hover:bg-transparent",
         destructive:
           "bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90",
         outline:
           "border border-slate-200 bg-white hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50",
         secondary:
-          "bg-secondary text-white p-2 border-2 ease-in-out duration-250 hover:border-secondary hover:text-secondary hover:bg-transparent",
+          "bg-secondary text-white p-2 border-2 border-secondary ease-in-out duration-250 hover:border-secondary hover:text-secondary hover:bg-transparent",
         ghost: "hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
         link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
       },
       size: {
         default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
+        sm: "h-7 rounded-md px-3 py-1 text-xs",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
       },
