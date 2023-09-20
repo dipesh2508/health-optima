d32cd6fa33639aa495233ff8974fd21076fc1e0d
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-09-20 20:28:18 +0530

    cta responsive and minor bugs

--- src/components/home/cta.tsx
+++ src/components/home/cta.tsx
@@ -25,20 +25,20 @@ const data = [
 ]
 const cta = () => {
     return (
-        <div className='bg-primary rounded-md shadow-custom m-16 py-12 flex flex-col items-center'>
+        <div className='bg-primary rounded-md shadow-custom px-3 mx-8 my-12 md:m-16 py-12 flex flex-col items-center'>
 
-            <h3 className='text-xl text-white font-regular'>Unlock your Potential</h3>
+            <h3 className='text-md md:text-xl text-white font-regular'>Unlock your Potential</h3>
 
-            <h1 className='text-4xl font-serif text-white font-semibold'>Harness the Power of our Applications</h1>
+            <h1 className='text-xl md:text-4xl font-serif text-center text-white font-semibold'>Harness the Power of our Applications</h1>
 
-            <div className='flex flex-row gap-8 justify-center mt-8'>
+            <div className='flex flex-col md:flex-row gap-8 justify-center mt-8'>
 
 
                 {data.map((item, index) => (
-                    <div className='relative' key={index}>
+                    <div className='relative group' key={index}>
                         <Image src={item.image} height={400} width={400} className='rounded grayscale hover:grayscale-0 ease-in-out delay-100' alt={item.title} />
-                        <div className='absolute bottom-0 text-center left-0 right-0 m-auto px-4 py-4 text-lg'>
-                            <h1 className='font-sans text-white font-semibold font-outline-2 text-4xl'>{item.title}</h1>
+                        <div className='group-hover:hidden absolute bottom-0 text-center left-0 right-0 m-auto px-4 py-12 md:py-4 text-lg'>
+                            <h1 className='font-sans text-black font-regular font-outline-2 text-2xl md:text-4xl'>{item.title}</h1>
                         </div>
                     </div>
                 ))}
--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -17,7 +17,7 @@ const hero = () => {
             <div className=" bg-black opacity-80 w-full min-h-[99vh] md:min-h-[122vh] absolute top-0 left-0 -z-40"></div>
             <div className=" w-full top-0 px-0 flex min-h-[99vh] md:min-h-[120vh] -mt-24 -z-30 flex-col items-center justify-center">
                 <div className="w-[90%] m-auto flex flex-col items-center gap-10">
-                    <h1 className="font-serif text-center text-2xl md:text-4xl lg:text-7xl leading-loose text-white">
+                    <h1 className="font-serif -z-20 text-center text-2xl md:text-4xl lg:text-7xl leading-loose text-white">
                         Empower Your Wellness  Journey <br /> with <span className="text-primary">Health Optima</span>
                     </h1>
                     <h3 className="text-white text-lg text-center md:text-3xl font-thin">Your Ultimate Health and Wellness Companion</h3>
