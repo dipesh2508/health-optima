1a247d69d587b4535968da43ce02e83007033d3e
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-09-22 12:07:36 +0530

    form initialization

---
+++ src/components/home/contact.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import Image from "next/image";
+import { useState } from "react";
+
+
+const UserValidation = z.object({
+    name: z.string().min(3).max(50),
+    email: z.string().email(),
+    message: z.string().min(10).max(500),
+});
+
+interface Props {
+    user: {
+        name: string;
+        email: string;
+        message: string;
+    };
+}
+
+const Contact = () => {
+    const form = useForm({
+        resolver: zodResolver(UserValidation),
+        mode: "onBlur",
+    });
+
+    const onSubmit = () => (data: z.infer<typeof UserValidation>) => {
+        console.log(data);
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+                <FormField control={form.control} name="name" render={({ field }) => (
+                    <FormItem className='flex flex-col gap-3 w-full'>
+                        <FormLabel className='text-light-1'>
+                            Name
+                        </FormLabel >
+                        <FormControl className='flex-1 text-base-semibold text-gray-200'>
+                            <Input type='text' className='account-form_input no focus' {...field} />
+                        </FormControl>
+                    </FormItem>)}
+                />
+                <FormField control={form.control} name="email" render={({ field }) => (
+                    <FormItem className='flex flex-col gap-3 w-full'>
+                        <FormLabel className='text-light-1'>
+                            Email
+                        </FormLabel >
+                        <FormControl className='flex-1 text-base-semibold text-gray-200'>
+                            <Input type='email' className='account-form_input no focus' {...field} />
+                        </FormControl>
+                    </FormItem>)}
+                />
+            </form>
+        </Form>
+    )
+
+}
+
+export default Contact;
