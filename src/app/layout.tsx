be884ff03bdfa0dfad0d99559cd06f4f1a9ff2fe
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-09-22 12:13:55 +0530

    sitemap installed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RJPXBG3GTS"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: ` window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-RJPXBG3GTS')`
+        }} />
+      </head>
       <body className={inter.className}>
         <NavBar />
         {children}
         <FootBar /></body>
-    </html>
+    </html >
   )
 }
