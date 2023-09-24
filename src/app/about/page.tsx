159bf3e909b7c96a602a9785c8759acd321dbe24
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-09-25 01:17:03 +0530

    about us page completed

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -58,8 +58,8 @@ const about = () => {
       {data.map((item, index) => (
           <div className='flex flex-col gap-1 items-center ease-in-out duration-400' key={index}>
 
-            <div className='w-72 h-80 -z-40 left-0 top-0 ease-in-out duration-400 bg-gradient-radial hover:bg-gradient-to-b from-blue-500 to-purple-500 rounded-tl-3xl rounded-br-3xl'>
-            <Image src={item.image} alt={item.name} className='-z-30 transition ease-in-out duration-400 w-72 h-80 scale-90 hover:scale-100 rounded-tl-3xl rounded-br-3xl' />
+            <div className='w-72 h-80 left-0 top-0 ease-in-out duration-400 bg-gradient-radial hover:bg-gradient-to-b from-blue-500 to-purple-500 rounded-tl-3xl rounded-br-3xl'>
+            <Image src={item.image} alt={item.name} className=' transition ease-in-out duration-400 w-72 h-80 scale-90 hover:scale-100 rounded-tl-3xl rounded-br-3xl' />
             </div>
             
               
@@ -79,6 +79,29 @@ const about = () => {
           </div>
           </div>
         ))}
+        </div>
+      </div>
+
+      <div className='m-4 md:m-16 p-4 bg-secondary rounded-lg'>
+        <div className='bg-white p-1 rounded-lg'>
+          <div className='bg-secondary p-2 md:p-4 rounded-lg flex flex-col items-center'>
+          <h1 className='text-4xl font-serif text-white'>About Us</h1>
+          <p className='text-white font-sans text-sm m-4 text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor justo lacinia, bibendum tellus a, ultricies magna. Etiam ex massa, ornare commodo lobortis sed, rutrum at elit. Aliquam malesuada ac neque id dignissim. Nullam id finibus velit, sed vulputate nisl. Integer et lorem elit. Etiam at mi augue. Curabitur porttitor sed urna ut elementum.<br></br><br></br>
+Nullam accumsan neque efficitur enim condimentum euismod. Mauris accumsan tortor ut massa facilisis volutpat. Nullam auctor urna eget ligula pellentesque dictum. Aenean sit amet efficitur mauris, et pretium nisl. Donec nec facilisis leo. Integer consequat dapibus sem, id porttitor lacus eleifend at. Mauris mauris tortor, faucibus eget diam sit amet, pharetra rhoncus nulla. Pellentesque feugiat dolor odio, vitae facilisis nulla hendrerit ac. Maecenas mattis nulla metus, a vulputate lacus rhoncus at. Cras neque nibh, commodo nec sem vitae, imperdiet ultrices arcu. Aenean efficitur hendrerit quam sit amet convallis. Pellentesque tempor ante non sollicitudin egestas. Sed ut eleifend augue, id posuere dui. Vestibulum nibh velit, tincidunt in lacus at, ullamcorper mollis orci.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='flex flex-col md:flex-row gap-8 md:gap-4 items-center my-12 mx-4 md:m-16'>
+        <div className='bg-primary rounded-lg p-4  text-white flex flex-col gap-4 items-center'>
+          <h3 className='bg-secondary mt-2 p-1 text-white font-sans'>WHO WE ARE, WHAT WE DO</h3>
+          <h1 className='font-serif text-3xl font-semibold'>OUR VISION</h1>
+          <p className='text-justify mt-0 m-4 md:m-8 font-sans text-black font-medium'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor justo lacinia, bibendum tellus a, ultricies magna. Etiam ex massa, ornare commodo lobortis sed, rutrum at elit. Aliquam malesuada ac neque id dignissim. Nullam id finibus velit, sed vulputate nisl. Integer et lorem elit. Etiam at mi augue. Curabitur porttitor sed urna ut elementum.</p>
+        </div>
+        <div className='bg-primary rounded-lg p-4 text-white flex flex-col gap-4 items-center'>
+          <h3 className='bg-secondary mt-2 p-1 text-white font-sans'>WHAT WE ASPIRE TO BE</h3>
+          <h1 className='font-serif text-3xl font-semibold'>OUR MISSION</h1>
+          <p className='text-justify mt-0 m-4 md:m-8 font-sans text-black font-medium'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor justo lacinia, bibendum tellus a, ultricies magna. Etiam ex massa, ornare commodo lobortis sed, rutrum at elit. Aliquam malesuada ac neque id dignissim. Nullam id finibus velit, sed vulputate nisl. Integer et lorem elit. Etiam at mi augue. Curabitur porttitor sed urna ut elementum.</p>
         </div>
       </div>
     </section>
