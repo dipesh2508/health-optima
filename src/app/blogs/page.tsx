0a1bc66aa89be03217d3eb6034c486e1e8fb8a31
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-09-28 10:44:46 +0530

    popular section - blog

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -8,42 +8,48 @@ import blog3 from "@/assets/images/blog 3.png";
 import blog4 from "@/assets/images/blog 4.png";
 
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 const popular = [
   {
     name: "Title of the Blog",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
     link: "",
-    image: popular1
+    image: popular1,
   },
   {
     name: "Title of the Blog",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
     link: "",
-    image: popular2
+    image: popular2,
   },
   {
     name: "Title of the Blog",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
     link: "",
-    image: popular3
-  }
+    image: popular3,
+  },
 ];
 
 const featured = [
   {
     category: "Category",
     name: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est. Aenean id imperdiet risus, vel fermentum...",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est. Aenean id imperdiet risus, vel fermentum...",
     link: "",
-    image: blog3
+    image: blog3,
   },
   {
     category: "Category",
     name: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est. Aenean id imperdiet risus, vel fermentum...",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est. Aenean id imperdiet risus, vel fermentum...",
     link: "",
-    image: blog4
+    image: blog4,
   },
 ];
 
@@ -51,61 +57,79 @@ const blog = [
   {
     category: "Category",
     name: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est. Aenean id imperdiet risus, vel fermentum...",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est. Aenean id imperdiet risus, vel fermentum...",
     link: "",
-    image: blog1
+    image: blog1,
   },
   {
     category: "Category",
     name: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est. Aenean id imperdiet risus, vel fermentum...",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est. Aenean id imperdiet risus, vel fermentum...",
     link: "",
-    image: blog2
+    image: blog2,
   },
   {
     category: "Category",
     name: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est. Aenean id imperdiet risus, vel fermentum...",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est. Aenean id imperdiet risus, vel fermentum...",
     link: "",
-    image: blog3
+    image: blog3,
   },
   {
     category: "Category",
     name: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est. Aenean id imperdiet risus, vel fermentum...",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est. Aenean id imperdiet risus, vel fermentum...",
     link: "",
-    image: blog4
-  }
-]
-
-
+    image: blog4,
+  },
+];
 
 const blogs = () => {
   return (
     <main>
-      <section>
-        <h1>Popular works</h1>
-        {popular.map((app, index) => (
-          <div key={index}>
-            <div className="">
-              <Image src={app.image} alt={app.name} className="rounded-full"/>
+      <section className="mx-28 my-12 px-2">
+        <h1 className="font-serif text-4xl font-semibold text-purple-950">
+          Popular works
+        </h1>
+        <div className="my-12 flex flex-row gap-4">
+          {popular.map((app, index) => (
+            <div key={index} className="flex flex-row gap-2">
+              <div className="grid content-center">
+                <Image
+                  height={121}
+                  width={121}
+                  src={app.image}
+                  alt={app.name}
+                  className="rounded-full"
+                />
+              </div>
+              <div className="flex flex-col">
+                <h2 className="font-serif text-2xl font-medium">{app.name}</h2>
+                <p className="font-sans text-sm font-light">
+                  {app.description}
+                </p>
+                <div>
+                  <Button className="mt-1" size={"sm"}>see</Button>
+                </div>
+              </div>
             </div>
-            <div><h2>{app.name}</h2>
-            <p>{app.description}</p></div>
-          </div>
-        ))}
+          ))}
+        </div>
       </section>
-      <section>
-        <h1>Featured</h1>
-        {featured.map((app, index) => (
+      <section className="mx-28 px-2">
+        <h1 className="font-serif text-4xl font-semibold text-secondary">Featured</h1>
+        {featured.map((item, index) => (
           <div key={index}>
-            <h2>{app.name}</h2>
-            <p>{app.description}</p>
+
           </div>
         ))}
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default blogs
+export default blogs;
