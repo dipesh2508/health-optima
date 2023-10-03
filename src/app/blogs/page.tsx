60803eca109c9992c495a6a0111258587b62cc27
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-10-03 20:15:22 +0530

    Revert "article regulated"
    This reverts commit 7d65b82d46c0e3a1eae461633a513c0aeb2cf0ec.

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -10,7 +10,6 @@ import blog4 from "@/assets/images/blog 4.png";
 import { FaTag } from "react-icons/fa";
 
 import Image from "next/image";
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 const popular = [
@@ -47,11 +46,11 @@ const featured = [
     image: blog3,
   },
   {
-    category: "Wellness",
-    name: "The Silent Struggle: Insomnia and How to Overcome It",
+    category: "Category",
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
     description:
-      "A pervasive sleep disorder that can have a devastating impact on your health, work, and relationships. Learn about the causes, effects, and practical solutions in this comprehensive blog.",
-    link: "/blogs/The-Silent-Struggle",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est. Aenean id imperdiet risus, vel fermentum...",
+    link: "",
     image: blog4,
   },
 ];
@@ -82,11 +81,11 @@ const blog = [
     image: blog3,
   },
   {
-    category: "Wellness",
-    name: "The Silent Struggle: Insomnia and How to Overcome It",
+    category: "Category",
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
     description:
-      "A pervasive sleep disorder that can have a devastating impact on your health, work, and relationships. Learn about the causes, effects, and practical solutions in this comprehensive blog.",
-    link: "/blogs/The-Silent-Struggle",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est. Aenean id imperdiet risus, vel fermentum...",
+    link: "",
     image: blog4,
   },
 ];
@@ -95,10 +94,10 @@ const blogs = () => {
   return (
     <main>
       <section className="mx-8 my-12 px-2 md:mx-28">
-        <h1 className="text-center font-serif text-2xl font-semibold text-purple-950 md:text-left md:text-4xl">
+        <h1 className="text-center font-serif text-2xl md:text-4xl font-semibold text-purple-950 md:text-left">
           Popular works
         </h1>
-        <div className="my-8 flex flex-col gap-4 md:my-12 md:flex-row">
+        <div className="my-8 md:my-12 flex flex-col gap-4 md:flex-row">
           {popular.map((app, index) => (
             <div key={index} className="flex flex-row gap-2 md:w-auto">
               <div className="grid content-center">
@@ -111,10 +110,8 @@ const blogs = () => {
                 />
               </div>
               <div className="flex flex-col">
-                <h2 className="font-serif text-lg font-medium md:text-2xl">
-                  {app.name}
-                </h2>
-                <p className="font-sans text-xs font-light md:text-sm">
+                <h2 className="font-serif text-lg md:text-2xl font-medium">{app.name}</h2>
+                <p className="font-sans text-xs md:text-sm font-light">
                   {app.description}
                 </p>
                 <div>
@@ -128,7 +125,7 @@ const blogs = () => {
         </div>
       </section>
       <section className="mx-8 px-2 md:mx-28">
-        <h1 className="text-center font-serif text-2xl font-semibold text-secondary md:text-left md:text-4xl">
+        <h1 className="text-center font-serif text-2xl md:text-4xl font-semibold text-secondary md:text-left">
           Featured
         </h1>
         <div>
@@ -165,7 +162,7 @@ const blogs = () => {
                   key={index}
                   className="grid grid-cols-1 align-middle md:grid-cols-3 md:content-center md:gap-3"
                 >
-                  <div className="grid items-center">
+                  <div className="flex flex-col items-center">
                     <Image
                       height={34}
                       width={300}
@@ -187,7 +184,6 @@ const blogs = () => {
                       {item.description}
                     </h1>
                     <div className="flex justify-center md:justify-start">
-                      <Link href={item.link}>
                       <Button
                         variant={"secondary"}
                         size={"sm"}
@@ -195,7 +191,6 @@ const blogs = () => {
                       >
                         Read
                       </Button>
-                      </Link>
                     </div>
                   </div>
                 </div>
@@ -208,7 +203,7 @@ const blogs = () => {
         <h1 className=" text-center font-serif text-2xl font-semibold text-black md:text-left md:text-5xl">
           ALL <span className="text-purple-950">BLOGS</span>
         </h1>
-        <div className="col-span-2 mt-8 flex flex-col items-center gap-12 md:mt-16">
+        <div className="col-span-2 mt-8 md:mt-16 flex flex-col items-center gap-12">
           {blog.map((item, index) => (
             <div
               key={index}
@@ -232,11 +227,11 @@ const blogs = () => {
                 <h1 className="font-serif text-lg font-semibold leading-6 md:text-4xl">
                   {item.name}
                 </h1>
-                <h1 className="mt-1 font-sans text-base font-light md:text-base">
+                <h1 className="mt-1 font-sans text-base font-light md:text-lg">
                   {item.description}
                 </h1>
                 <div className="flex justify-center md:justify-start">
-                  <Link href={item.link}><Button className="mt-2">Read</Button></Link>
+                  <Button className="mt-2">Read</Button>
                 </div>
               </div>
             </div>
