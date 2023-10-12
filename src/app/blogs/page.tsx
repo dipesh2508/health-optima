531e4644eee5179e483c0f1b514574cf8bcacfb3
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-10-13 04:20:06 +0530

    blogs page cleaned

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,97 +1,14 @@
-import popular1 from "@/assets/images/popular1.jpg";
-import popular2 from "@/assets/images/popular2.jpg";
-import popular3 from "@/assets/images/popular3.jpg";
-
 import run from "@/assets/images/run.jpg";
 
-import blog1 from "@/assets/images/blog 1.png";
-import blog2 from "@/assets/images/blog 2.png";
-import blog3 from "@/assets/images/blog 3.png";
-import blog4 from "@/assets/images/blog 4.png";
+import { blog } from "@/constants/blogs";
+import { featured } from "@/constants/blogs";
+import { popular } from "@/constants/blogs";
 
 import { FaTag } from "react-icons/fa";
 
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-
-const popular = [
-  {
-    name: "Title of the Blog",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
-    link: "",
-    image: popular1,
-  },
-  {
-    name: "Title of the Blog",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
-    link: "",
-    image: popular2,
-  },
-  {
-    name: "Title of the Blog",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
-    link: "",
-    image: popular3,
-  },
-];
-
-const featured = [
-  {
-    category: "Category",
-    name: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est. Aenean id imperdiet risus, vel fermentum...",
-    link: "",
-    image: blog3,
-  },
-  {
-    category: "Wellness",
-    name: "The Silent Struggle: Insomnia and How to Overcome It",
-    description:
-      "A pervasive sleep disorder that can have a devastating impact on your health, work, and relationships. Learn about the causes, effects, and practical solutions in this comprehensive blog.",
-    link: "/blogs/The-Silent-Struggle",
-    image: blog4,
-  },
-];
-
-const blog = [
-  {
-    category: "Category",
-    name: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est. Aenean id imperdiet risus, vel fermentum...",
-    link: "",
-    image: blog1,
-  },
-  {
-    category: "Category",
-    name: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est. Aenean id imperdiet risus, vel fermentum...",
-    link: "",
-    image: blog2,
-  },
-  {
-    category: "Category",
-    name: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra magna sed pretium sagittis. Nullam ultrices nec ipsum non vestibulum. Proin a tincidunt est. Aenean id imperdiet risus, vel fermentum...",
-    link: "",
-    image: blog3,
-  },
-  {
-    category: "Wellness",
-    name: "The Silent Struggle: Insomnia and How to Overcome It",
-    description:
-      "A pervasive sleep disorder that can have a devastating impact on your health, work, and relationships. Learn about the causes, effects, and practical solutions in this comprehensive blog.",
-    link: "/blogs/The-Silent-Struggle",
-    image: blog4,
-  },
-];
 
 const blogs = () => {
   return (
