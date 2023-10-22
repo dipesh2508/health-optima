1d66db0b811baf3c964d9742ee474088aac75277
Preyanshu <preyanshudhapola6184@gmail.com>
2023-10-22 23:00:44 +0530

    sleep-tracker-normal

--- src/app/apps/sleepTracker/page.js
+++ src/app/apps/sleepTracker/page.js
@@ -6,10 +6,12 @@ const page = () => {
   return (
     <>
       <h1 className="mt-8 text-center text-4xl font-medium">Sleep Tracker</h1>
-      <div className=" mt-8 h-auto w-28">
-        <div className=" m-4 box-border flex justify-evenly rounded-3xl border-2 bg-purple-600 p-6">
+      <div className=" mt-8 h-auto ">
+        <div className=" m-4 box-border flex justify-evenly rounded-3xl border-2 p-6">
           <div className="flex flex-col items-center border-2 p-6">
-            <h1 className="text-center text-4xl font-bold">DAILY</h1>
+            
+            <h1 className="text-center text-4xl font-bold">DAILY</h1>]
+
             <form>
               <p className=" m-2 bg-white p-4 text-center text-3xl font-semibold text-black">
                 XX : XX : XX
@@ -46,19 +48,21 @@ const page = () => {
             </Button>
 
             </form>
+
           </div>
 
           <div className="border-2 m-4">
-            
+            <div>
                 <h1 className="text-2xl p-3 font-semibold">Last 7 days Average Duration: <input ></input></h1>
+
                 <div>
-                    <h1 className="text-2xl font-semibold m-4">Last 7 days:</h1>
-                    
+                    <h1 className="text-2xl font-semibold m-4">Last 7 days:</h1> 
                 </div>
+
                 <div className="border-2">
                  fetch the data herer
                 </div>
-            
+            </div>
           </div>
         </div>
       </div>
