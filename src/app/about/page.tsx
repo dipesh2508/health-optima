1f24b25a39f34793bba1e780f503eef35e9d9464
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-10-30 21:29:31 +0530

    added profile data constant

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,4 @@
-import Dipesh from "@/assets/images/dipesh.png";
-import Isheta from "@/assets/images/isheta.png";
-import Divyanshu from "@/assets/images/divyanshu.png";
-import Preyanshu from "@/assets/images/preyanshu.png";
+import {data} from "@/constants/profile"
 
 import { BsLinkedin } from "react-icons/bs";
 import { BsGithub } from "react-icons/bs";
@@ -9,45 +6,6 @@ import { BsInstagram } from "react-icons/bs";
 
 import Image from "next/image";
 import Link from "next/link";
-
-const data = [
-  {
-    name: "Dipesh Ranjan",
-    image: Dipesh,
-    team: "Team Lead",
-    role: "Front-End Developer and UI/UX Designer",
-    linkedin: "https://www.linkedin.com/in/dipesh-ranjan/",
-    instagram: "https://www.instagram.com/dipesh_ranjan07/",
-    github: "https://github.com/dipesh2508",
-  },
-  {
-    name: "Isheta Aggarwal",
-    image: Isheta,
-    team: "Team Member",
-    role: "Research Analyst and Copywriter",
-    linkedin: "https://www.linkedin.com/in/isheta-aggarwal/",
-    instagram: "https://www.instagram.com/isheta20/",
-    github: "https://github.com/Isheta20",
-  },
-  {
-    name: "Divyanshu Agarwal",
-    image: Divyanshu,
-    team: "Team Member",
-    role: "Backend Developer",
-    linkedin: "https://www.linkedin.com/in/divyanshu-agarwal-15774b222/",
-    instagram: "https://www.instagram.com/divyanshu_agarwal8/",
-    github: "https://github.com/Divyanshu2309",
-  },
-  {
-    name: "Preyanshu Dapola",
-    image: Preyanshu,
-    team: "Team Member",
-    role: "Front-End Developer and Keyframe Animator",
-    linkedin: "https://www.linkedin.com/in/preyanshu-d-852019231/",
-    instagram: "https://www.instagram.com/dpreyanshu/",
-    github: "https://github.com/sickCoder6184",
-  },
-];
 
 const about = () => {
   return (
---
+++ src/constants/profile.ts
@@ -0,0 +1,43 @@
+import Dipesh from "@/assets/images/dipesh.png";
+import Isheta from "@/assets/images/isheta.png";
+import Divyanshu from "@/assets/images/divyanshu.png";
+import Preyanshu from "@/assets/images/preyanshu.png";
+
+export const data = [
+    {
+      name: "Dipesh Ranjan",
+      image: Dipesh,
+      team: "Team Lead",
+      role: "Front-End Developer and UI/UX Designer",
+      linkedin: "https://www.linkedin.com/in/dipesh-ranjan/",
+      instagram: "https://www.instagram.com/dipesh_ranjan07/",
+      github: "https://github.com/dipesh2508",
+    },
+    {
+      name: "Isheta Aggarwal",
+      image: Isheta,
+      team: "Team Member",
+      role: "Research Analyst and Copywriter",
+      linkedin: "https://www.linkedin.com/in/isheta-aggarwal/",
+      instagram: "https://www.instagram.com/isheta20/",
+      github: "https://github.com/Isheta20",
+    },
+    {
+      name: "Divyanshu Agarwal",
+      image: Divyanshu,
+      team: "Team Member",
+      role: "Backend Developer",
+      linkedin: "https://www.linkedin.com/in/divyanshu-agarwal-15774b222/",
+      instagram: "https://www.instagram.com/divyanshu_agarwal8/",
+      github: "https://github.com/Divyanshu2309",
+    },
+    {
+      name: "Preyanshu Dapola",
+      image: Preyanshu,
+      team: "Team Member",
+      role: "Front-End Developer and Keyframe Animator",
+      linkedin: "https://www.linkedin.com/in/preyanshu-d-852019231/",
+      instagram: "https://www.instagram.com/dpreyanshu/",
+      github: "https://github.com/sickCoder6184",
+    },
+  ];
