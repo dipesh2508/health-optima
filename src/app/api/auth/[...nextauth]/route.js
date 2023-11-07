f5250dc6fd97be5cbcb373f19a2b52ee60e9ce31
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-11-07 19:41:55 +0530

    renamed env conventions

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -3,8 +3,8 @@ import GoogleProvider from "next-auth/providers/google"
 const handler = NextAuth({
   providers:[
     GoogleProvider({
-        clientId:process.env.GOOGLE_CLIENT_ID,
-        clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+        clientId:process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+        clientSecret:process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
     })
   ]
 })
