d7988883fdd31c992c8ccc5320e2864eb10b0f99
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-11-07 19:52:30 +0530

    Added Secret

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -3,10 +3,11 @@ import GoogleProvider from "next-auth/providers/google"
 const handler = NextAuth({
   providers:[
     GoogleProvider({
-        clientId:process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-        clientSecret:process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+        clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+        clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
     })
-  ]
+  ],
+  secret: process.env.NEXT_PUBLIC_SECRET
 })
 
 export { handler as GET, handler as POST }
