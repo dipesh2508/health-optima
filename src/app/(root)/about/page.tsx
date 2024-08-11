ab4926f5538d93e2f76050857106d4fde7bc807d
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-08-12 00:31:40 +0530

    design:about page made responsive

--- src/app/(root)/about/page.tsx
+++ src/app/(root)/about/page.tsx
@@ -20,8 +20,8 @@ const variants = {
 const about = () => {
   return (
     <section className="flex flex-col justify-center bg-zinc-50 font-serif">
-      <div className="grid grid-cols-3 items-center gap-12 px-32 py-10">
-        <div className="col-span-1 text-5xl">
+      <div className="grid grid-cols-1 items-center gap-6 px-12 py-6 md:grid-cols-3 md:gap-12 md:px-32 md:py-10">
+        <div className="col-span-1 text-2xl md:text-5xl">
           <MotionDiv
             variants={variants}
             initial="hidden"
@@ -35,7 +35,9 @@ const about = () => {
           >
             We are
             <br />
-            <h1 className="font-semibold text-dark-primary">HEALTH OPTIMA</h1>
+            <h1 className="font-bold text-dark-primary md:font-semibold">
+              HEALTH OPTIMA
+            </h1>
           </MotionDiv>
         </div>
         <MotionDiv
@@ -48,15 +50,15 @@ const about = () => {
           }}
           whileInView={{ opacity: 1 }}
           viewport={{ once: true, amount: 0.5 }}
-          className="col-span-2 flex flex-col gap-6 pl-8 pr-8"
+          className="flex flex-col gap-2 md:col-span-2 md:gap-6 md:px-8"
         >
           <div className="flex items-end gap-2">
-            <h2 className="text-5xl font-medium">
+            <h2 className="text-2xl font-bold md:text-5xl md:font-medium">
               About <span className="text-dark-primary">U</span>s
             </h2>
-            <div className="mb-2 h-1 w-64 bg-teal-950"></div>
+            <div className="mb-2 h-0.5 w-64 bg-teal-950 md:h-1"></div>
           </div>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-2 text-base md:text-sm">
             <p>
               At HealthOptima, we&#39;re dedicated to promoting comprehensive
               well-being through a diverse range of health-centric resources and
@@ -84,7 +86,7 @@ const about = () => {
         }}
         whileInView={{ opacity: 1 }}
         viewport={{ once: true, amount: 0.5 }}
-        className="mx-auto rounded-md px-28 pb-12"
+        className="mx-auto rounded-md px-8 pb-4 md:px-28 md:pb-12"
       >
         <Image src={About} alt="About" className="rounded-md" />
       </MotionDiv>
@@ -98,7 +100,7 @@ const about = () => {
         }}
         whileInView={{ opacity: 1 }}
         viewport={{ once: true, amount: 0.5 }}
-        className="flex flex-col gap-4 px-80 pb-12 text-center"
+        className="flex flex-col gap-4 px-12 pb-8 text-center text-sm md:px-80 md:pb-12 md:text-base"
       >
         <p>
           But HealthOptima goes beyond knowledge. Our suite of applications,
@@ -122,8 +124,8 @@ const about = () => {
           duration: 0.4,
         }}
         whileInView={{ opacity: 1 }}
-        viewport={{ once: true, amount: 0.5 }}
-        className="mx-28 grid grid-cols-3 gap-16 rounded-sm bg-purple-100 px-20 py-16"
+        viewport={{ once: true, amount: 0.2 }}
+        className="mx-4 md:mx-28 grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-16 rounded-sm bg-purple-100 px-10 py-8 md:px-20 md:py-16"
       >
         <MotionDiv
           variants={variants}
@@ -135,11 +137,11 @@ const about = () => {
           }}
           whileInView={{ opacity: 1 }}
           viewport={{ once: true, amount: 0.5 }}
-          className="col-span-2 flex flex-col items-center"
+          className="col-span-1 md:col-span-2 flex flex-col items-center"
         >
           <div className="my-auto flex flex-col gap-4">
-            <h2 className="text-5xl">Our Mission</h2>
-            <p>
+            <h2 className="text-2xl md:text-5xl text-center">Our Mission</h2>
+            <p className="text-sm md:text-base text-center">
               Our mission is to empower individuals on their path to holistic
               wellness. We combine advanced technology with personalized
               experiences to simplify health management, inspire positive
@@ -147,7 +149,7 @@ const about = () => {
               trusted companion in achieving optimal well-being, one step at a
               time.
             </p>
-            <div className="flex flex-row gap-3 text-xl">
+            <div className="flex flex-row gap-1 md:gap-3 text-base md:text-xl text-center">
               <h3 className="text-secondary">Join us, in our Mission.</h3>
               <Link href="/sign-up" className="text-dark-primary underline">
                 Register Here
@@ -193,11 +195,11 @@ const about = () => {
           }}
           whileInView={{ opacity: 1 }}
           viewport={{ once: true, amount: 0.5 }}
-          className="col-span-2 flex flex-col items-center"
+          className="col-span-1 md:col-span-2 flex flex-col items-center"
         >
           <div className="my-auto flex flex-col gap-4">
-            <h2 className="text-5xl">Our Vision</h2>
-            <p>
+            <h2 className="text-2xl md:text-5xl text-center">Our Vision</h2>
+            <p className="text-sm md:text-base  text-center">
               Our vision is to pioneer a profound transformation in the way
               individuals approach their health and wellness. We envision a
               world where people are not only well-informed but also genuinely
@@ -226,17 +228,17 @@ const about = () => {
           duration: 0.4,
         }}
         whileInView={{ opacity: 1 }}
-        viewport={{ once: true, amount: 0.5 }}
-        className="flex flex-col gap-12 px-28 py-24"
+        viewport={{ once: true, amount: 0.2 }}
+        className="flex flex-col gap-6 md:gap-12 px-12 py-8 md:px-28 md:py-24"
       >
         <div className="flex items-end gap-2">
-          <h2 className="text-4xl font-medium">
-            Meet Our Team of Very Capable <br />
+          <h2 className="text-lg md:text-4xl font-semibold md:font-medium">
+            <span className="text-3xl md:text-xl">Meet Our Team</span> of Very Capable<br />
             Individuals in the field of Technology
           </h2>
           <div className="mb-2 h-1 w-64 bg-black" />
         </div>
-        <div className="grid grid-cols-4 gap-12">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-12">
           <MotionDiv
             variants={variants}
             initial="hidden"
@@ -256,9 +258,9 @@ const about = () => {
                 className="rounded-md transition-all duration-300 ease-in-out hover:scale-105"
               />
             </Link>
-            <div className="flex flex-col gap-2">
-              <h3 className="text-2xl font-bold">Dipesh Ranjan</h3>
-              <p className="font-sans text-lg leading-normal">
+            <div className="flex flex-col md:gap-2 text-center">
+              <h3 className="text-xl md:text-2xl font-bold">Dipesh Ranjan</h3>
+              <p className="font-sans md:text-lg text-sm leading-normal">
                 Full-Stack Developer & UX Designer
               </p>
             </div>
@@ -282,9 +284,9 @@ const about = () => {
                 className="rounded-md transition-all duration-300 ease-in-out hover:scale-105"
               />
             </Link>
-            <div className="flex flex-col gap-2">
-              <h3 className="text-2xl font-bold">Isheta Aggarwal</h3>
-              <p className="font-sans text-lg leading-normal">
+            <div className="flex flex-col gap-2 text-center">
+              <h3 className="text-xl md:text-2xl font-bold">Isheta Aggarwal</h3>
+              <p className="font-sans md:text-lg text-sm leading-normal">
                 Front-End Developer & UX Designer
               </p>
             </div>
@@ -308,9 +310,9 @@ const about = () => {
                 className="rounded-md transition-all duration-300 ease-in-out hover:scale-105"
               />
             </Link>
-            <div className="flex flex-col gap-2">
-              <h3 className="text-2xl font-bold">Preyanshu Dhapola</h3>
-              <p className="font-sans text-lg leading-normal">
+            <div className="flex flex-col gap-2 text-center">
+              <h3 className="text-xl md:text-2xl font-bold">Preyanshu Dhapola</h3>
+              <p className="font-sans md:text-lg text-sm leading-normal">
                 Front-End Developer
               </p>
             </div>
@@ -334,9 +336,9 @@ const about = () => {
                 className="rounded-md transition-all duration-300 ease-in-out hover:scale-105"
               />
             </Link>
-            <div className="flex flex-col gap-2">
-              <h3 className="text-2xl font-bold">Divyanshu Agarwal</h3>
-              <p className="font-sans text-lg leading-normal">
+            <div className="flex flex-col gap-2 text-center">
+              <h3 className="text-xl md:text-2xl font-bold">Divyanshu Agarwal</h3>
+              <p className="font-sans md:text-lg text-sm leading-normal">
                 Back-End Developer
               </p>
             </div>
@@ -354,9 +356,9 @@ const about = () => {
         }}
         whileInView={{ opacity: 1 }}
         viewport={{ once: true, amount: 0.5 }}
-        className="flex flex-row items-end gap-4 px-28 pb-24"
+        className="flex flex-col md:flex-row items-end justify-center md:gap-4 px-8 md:px-28 pb-24"
       >
-        <Image src={TNT} alt="Try N Test" className="w-96" />
+        <Image src={TNT} alt="Try N Test" className="w-96 mx-auto" />
         <MotionDiv
           variants={variants}
           initial="hidden"
@@ -367,7 +369,7 @@ const about = () => {
           }}
           whileInView={{ opacity: 1 }}
           viewport={{ once: true, amount: 0.5 }}
-          className="mb-8 h-1 w-96 bg-black"
+          className="mb-8 h-0.5 md:h-1 mx-auto w-48 md:w-96 bg-black"
         />
         <MotionDiv
           variants={variants}
@@ -379,7 +381,7 @@ const about = () => {
           }}
           whileInView={{ opacity: 1 }}
           viewport={{ once: true, amount: 0.5 }}
-          className="mb-6 flex flex-col gap-4 text-4xl"
+          className="mb-6 flex flex-col gap-4 text-center md:text-left text-xl md:text-4xl"
         >
           <h3>The Project is Made under the Organisation Try N&#39; Test</h3>
           <h3 className="font-semibold italic">
