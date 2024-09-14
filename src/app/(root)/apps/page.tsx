a406b1f5fd653b98cd8e1cf814bfdc6ea9584de5
Isheta <aggarwalisheta20@gmail.com>
2024-09-15 00:20:09 +0530

    design: Modified layout of apps page

--- src/app/(root)/apps/page.tsx
+++ src/app/(root)/apps/page.tsx
@@ -10,7 +10,7 @@ import { redirect } from "next/navigation";
 
 const data = [
   {
-    name: "Rest Assured",
+    name: "Sleep Tracker",
     tagline: "Your Trusted Partner for Optimal Sleep Health",
     description:
       "Experience the science of sound sleep with our intuitive Sleep Tracker, providing personalized insights for a rejuvenated you.",
@@ -26,7 +26,7 @@ const data = [
     image: BMI,
   },
   {
-    name: "Aquatrack",
+    name: "Water Tracker",
     tagline: "Your Hydration Companion",
     description:
       "Stay hydrated with ease. Track your daily water intake and level up your wellness journey with personalized hydration goals",
@@ -34,7 +34,7 @@ const data = [
     image: Water,
   },
   {
-    name: "TaskMinder",
+    name: "Task Log",
     tagline: "Where Tasks find their place",
     description:
       "Organize, prioritize, and conquer your day with our intuitive to-do list app. Your ultimate productivity partner is here.",
@@ -52,33 +52,34 @@ const Apps = () => {
   }
 
   return (
-    <section className="m-12 my-8 md:mx-64">
-      <h1 className="text-center font-serif text-4xl font-semibold text-primary md:text-6xl">
+    <section className="grid grid-cols-1 sm:grid-cols-2 gap-x-10 m-12 my-16 md:px-20">
+      <h1 className="sm:col-span-2 text-center font-serif text-4xl font-semibold text-primary md:text-5xl">
         Our Applications
       </h1>
+      <h2 className="text-black font-medium text-center text-lg sm:col-span-2 mt-4 mb-2">Choose an App to Enhance Your Day-to-Day Life</h2>
       {data.map((app, index) => (
         <div
           key={index}
-          className="my-8 flex flex-col items-center gap-1 md:flex-row md:items-start md:gap-4"
+          className="my-6 hover:scale-105 transition duration-300 flex flex-col items-center md:flex-row md:items-start md:gap-4 p-4 bg-white shadow-lg"
         >
           <Image
             src={app.image}
             alt={app.name}
-            height={250}
-            width={250}
+            height={248}
+            width={248}
             loading="lazy"
             className="rounded-lg shadow-lg"
           />
           <div className="flex flex-col items-center gap-2 md:items-start md:gap-4">
-            <h2 className="mt-4 font-serif text-3xl font-semibold text-secondary md:text-5xl">
+            <h2 className="mt-4 font-serif text-xl font-semibold text-secondary md:text-3xl min-h-8">
               {app.name}
             </h2>
-            <h3 className="text-center font-sans text-xl font-light md:text-start md:text-3xl">
+            <h3 className="text-center font-sans text-xl font-light md:text-start md:text-2xl min-h-14">
               {app.tagline}
             </h3>
-            <p className="text-center font-sans text-sm font-normal md:text-start md:text-xl">
+            <div className="text-center font-sans text-sm font-normal md:text-start md:text-sm min-h-16">
               {app.description}
-            </p>
+            </div>
             <div>
               <Link href={app.link}>
               <Button size="sm">See</Button>
