7da7cc17c6adda32a8ea3a758cc6e5b06bd195dd
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-10-15 02:53:39 +0530

    breaking-change:added public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,8 @@ import { authMiddleware } from "@clerk/nextjs/server";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/blogs","/blogs(.*)", "/about"],
+  publicRoutes: ["/", "/blogs","/blogs(.*)", "/api/webhook/clerk", "/about"],
+  ignoredRoutes: ["api/webhook/clerk"],
 });
 
 export const config = {
