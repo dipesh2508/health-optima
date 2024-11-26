e4299e3be584867d27074c74c82caf66006acadb
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-11-27 00:36:07 +0530

    design:fix homepage blog section responsiveness

--- src/components/home/blog.tsx
+++ src/components/home/blog.tsx
@@ -17,8 +17,11 @@ const Blog = async () => {
 
       <div className="content center mx-12 my-14 grid grid-cols-1 gap-12 md:mx-24 md:mt-28 md:grid-cols-2">
         {limitedPosts.map((post:any) => (
-          <Card key={post._id} className="border-none shadow-custom flex flex-row overflow-hidden h-[200px] transition-all duration-300 hover:shadow-xl hover:-translate-y-1">
-            <div className="w-[40%] relative">
+          <Card 
+            key={post._id} 
+            className="border-none shadow-custom flex flex-col md:flex-row overflow-hidden h-auto md:h-[200px] transition-all duration-300 hover:shadow-xl hover:-translate-y-1"
+          >
+            <div className="w-full h-[200px] md:w-[40%] md:h-auto relative">
               <Image
                 src={post.coverImage}
                 alt={post.title}
@@ -27,19 +30,19 @@ const Blog = async () => {
                 loading="lazy"
               />
             </div>
-            <div className="w-[60%] flex flex-col justify-between p-4">
+            <div className="w-full md:w-[60%] flex flex-col justify-between p-4">
               <div className="space-y-2">
-                <p className="font-sans text-base font-light text-muted-foreground">
+                <p className="font-sans text-sm md:text-base font-light text-muted-foreground">
                   {post.category}
                 </p>
-                <h2 className="font-serif text-xl font-semibold line-clamp-2">
+                <h2 className="font-serif text-lg md:text-xl font-semibold line-clamp-2">
                   {post.title}
                 </h2>
-                <p className="font-sans text-sm font-light line-clamp-2 text-muted-foreground">
+                <p className="font-sans text-xs md:text-sm font-light line-clamp-2 text-muted-foreground">
                   {post.description}
                 </p>
               </div>
-              <div className="flex justify-start mt-auto">
+              <div className="flex justify-start mt-4 md:mt-auto">
                 <Link href={`/blogs/${post._id}`}>
                   <Button variant={"secondary"} size={"sm"}>
                     Read
