fda7d2dedba7eba026d86f2774500ea5b3e2b53c
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2025-01-10 17:46:56 +0530

    feat:added userdetails hook

---
+++ src/hooks/useUserDetails.ts
@@ -0,0 +1,58 @@
+import { getUserByClerkId } from "@/lib/actions/user.actions";
+import { useAuth } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
+
+interface UserDetails {
+  userId: string;
+  clerkId: string;
+  name: string;
+  username: string;
+  profileImage: string;
+  email: string;
+  isLoading: boolean;
+  error: Error | null;
+}
+
+export function useUserDetails(): UserDetails {
+  const { userId: clerkId } = useAuth();
+  const [userDetails, setUserDetails] = useState<UserDetails>({
+    userId: "",
+    clerkId: "",
+    name: "",
+    username: "",
+    profileImage: "",
+    email: "",
+    isLoading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      if (!clerkId) return;
+
+      try {
+        const user = await getUserByClerkId(clerkId);
+        setUserDetails({
+          userId: user._id,
+          clerkId: user.clerkId,
+          name: user.name,
+          username: user.username,
+          profileImage: user.profileImage,
+          email: user.email,
+          isLoading: false,
+          error: null,
+        });
+      } catch (error) {
+        setUserDetails((prev) => ({
+          ...prev,
+          isLoading: false,
+          error: error as Error,
+        }));
+      }
+    };
+
+    fetchUserDetails();
+  }, [clerkId]);
+
+  return userDetails;
+}
