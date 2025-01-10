82535848ccd5be280dd2af118176a4457317ada0
Isheta <aggarwalisheta20@gmail.com>
2025-01-10 20:57:32 +0530

    feat: added create a new list functionality and view the lists created in the sidebar

--- src/components/apps/taskList/Sidebar.tsx
+++ src/components/apps/taskList/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Label } from "@/components/ui/label";
 import {
   Dialog,
@@ -20,24 +20,91 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import BorderBox from "@/components/shared/BorderBox";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useApi } from "@/hooks/useApi";
+import { useUserDetails } from "@/hooks/useUserDetails";
+import Loading from "@/app/(root)/loading";
 
-const listNames = [
-  { title: "Grocerry" },
-  { title: "Study bla bal blaljfoijiorjggeio" },
-  { title: "Work" },
-  { title: "Gym" },
-  { title: "I" },
-];
+const formSchema = z.object({
+  newListTitle: z.string().min(1, {
+    message: "Title must be at least 1 character.",
+  }),
+});
+type fieldType = z.infer<typeof formSchema>;
+
+interface ListNames {
+  userid: string;
+  listname: string;
+}
+
+interface TaskList {
+  _id: string;
+  userId: string;
+  listName: string | null;
+  taskIds: [];
+  createdAt: string | null;
+  updatedAt: string | null;
+  __v: number;
+}
 
 const Sidebar = () => {
-  const [listName, setListName] = useState("");
   const [open, setOpen] = useState(false);
-  function handleSubmit(e: React.FormEvent) {
-    e.preventDefault();
-    //connect to backend
+  const [url, setUrl] = useState("");
 
+  const form = useForm<fieldType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      newListTitle: "",
+    },
+  });
+
+  const userDetails = useUserDetails();
+  const { error, isLoading, mutate } = useApi("/api/taskList", {
+    method: "POST",
+    onSuccess: (data) => {
+      console.log("New List created successfully", data);
+    },
+    onError: (error) => {
+      console.error("Failed to create new list", error);
+    },
+  });
+
+  function onSubmit(data: fieldType) {
+    console.log(data);
+    mutate({
+      body: { userId: userDetails.userId, listName: data.newListTitle },
+    });
     setOpen(false);
   }
+
+  useEffect(() => {
+    if (isLoading) {
+      <Loading />;
+    } else {
+      setUrl(`/api/taskList?userId=${userDetails.userId}`);
+    }
+  }, [isLoading, userDetails.userId]);
+
+  const {
+    data: listNames,
+    error: fetchError,
+    isLoading: FetchisLoading,
+  } = useApi(url);
+  // if (fetchError) return <p>Error: {fetchError.message}</p>;
+  console.log(listNames);
+  const listNamesString = JSON.parse(JSON.stringify(listNames, null, 2));
+  console.log(listNamesString);
 
   return (
     <div className="sidebar col-span-1 flex flex-col justify-between border-r-2 border-primary-2 bg-white p-6">
@@ -50,16 +117,19 @@ const Sidebar = () => {
               size="sm"
               className="group hover:bg-primary-4"
             >
-              {/* <GiHamburgerMenu className="text-primary-6"/> */}
               <RxHamburgerMenu className="text-lg text-primary-10 group-hover:text-white" />
             </Button>
           </CollapsibleTrigger>
         </div>
         <CollapsibleContent>
           <div className="flex flex-col gap-1">
-            {listNames.map((it, index) => {
-              return <BorderBox key={index}>{it.title}</BorderBox>;
-            })}
+            {!listNamesString?.lists?.length ? (
+              <BorderBox key={0}>No List created</BorderBox>
+            ) : (
+              listNamesString.lists.map((it: TaskList) => {
+                return <BorderBox key={it._id}>{it.listName}</BorderBox>;
+              })
+            )}
           </div>
         </CollapsibleContent>
       </Collapsible>
@@ -80,22 +150,33 @@ const Sidebar = () => {
               Choose a title for your new list!
             </DialogTitle>
           </DialogHeader>
-          <form onSubmit={handleSubmit}>
-            <div className="flex flex-col items-start gap-4">
-              <Label htmlFor="title" className="text-left">
-                New List Title
-              </Label>
-              <Input
-                id="title"
-                placeholder="New List"
-                className="col-span-3"
-                onChange={(e) => setListName(e.target.value)}
+          <DialogDescription></DialogDescription>
+
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="newListTitle"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>New List Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder="New List" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Let&apos;s plan a new list!
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
-            </div>
-            <DialogFooter>
-              <Button type="submit">Create</Button>
-            </DialogFooter>
-          </form>
+              <DialogFooter>
+                <Button type="submit">
+                  {isLoading ? "Creating..." : "Create"}
+                </Button>
+              </DialogFooter>
+            </form>
+          </Form>
         </DialogContent>
       </Dialog>
     </div>
