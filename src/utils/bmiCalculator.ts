25deda91da8f8a97fbbcca3f2956f76fdedb8800
Isheta <aggarwalisheta20@gmail.com>
2025-01-31 14:04:49 +0530

    refactor: Removed the jsdocs

--- src/utils/bmiCalculator.ts
+++ src/utils/bmiCalculator.ts
@@ -1,13 +1,5 @@
 import { metric } from "@trifoia/bmi-percentile-calculator";
 
-/**
- * Calculates BMI and BMI Percentile using metric units.
- * @param weight Weight in kilograms
- * @param height Height in centimeters
- * @param ageInMonths Age in months
- * @param sex "m" for male, "f" for female
- * @returns Object containing BMI and BMI percentile
- */
 export const calculateBMIAndPercentile = (
   weight: number,
   height: number,
